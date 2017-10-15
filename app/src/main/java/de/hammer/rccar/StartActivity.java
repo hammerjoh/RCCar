8d2324ffc9b50423b6d4dd0aa7574cc205510761
Hammer <johanneshammer15@googlemail.com>
2017-10-15 16:08:43 +0200

    -

--- app/src/main/java/de/hammer/rccar/StartActivity.java
+++ app/src/main/java/de/hammer/rccar/StartActivity.java
@@ -1,17 +1,23 @@
 package de.hammer.rccar;
 
-import android.preference.EditTextPreference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 
 public class StartActivity extends AppCompatActivity {
+
+    public float laenge;
+    public float breite;
+    public float hoehe;
+    public String flaeche;
+    public String volumen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,7 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
         final EditText va_laenge = (EditText) findViewById(R.id.va_laenge);
         final EditText va_breite = (EditText) findViewById(R.id.va_breite);
+        final EditText va_hoehe = (EditText) findViewById(R.id.va_hoehe);
         final TextView va_flaeche = (TextView) findViewById(R.id.tv_result);
         Button btn_berechne = (Button)findViewById(R.id.btn_berechne);
         btn_berechne.setOnClickListener(new View.OnClickListener() {
@@ -27,14 +34,21 @@ public class StartActivity extends AppCompatActivity {
                 if (va_breite.getText().toString().isEmpty()||va_laenge.getText().toString().isEmpty())
                 {
                     va_flaeche.setText("");
-                    if(va_breite.getText().toString().isEmpty()) Toast.makeText(getApplicationContext(),"Bitte Breite eingeben!",Toast.LENGTH_SHORT).show();
-                    if(va_laenge.getText().toString().isEmpty()) Toast.makeText(getApplicationContext(),"Bitte Länge eingeben!",Toast.LENGTH_SHORT).show();
+                    if(va_breite.getText().toString().isEmpty()) Toast.makeText(getApplicationContext(),R.string.breite_eingeben,Toast.LENGTH_SHORT).show();
+                    if(va_laenge.getText().toString().isEmpty()) Toast.makeText(getApplicationContext(),R.string.laenge_eingeben,Toast.LENGTH_SHORT).show();
+                }
+                else if (va_hoehe.getText().toString().isEmpty()){
+                    laenge = Float.parseFloat(va_laenge.getText().toString());
+                    breite = Float.parseFloat(va_breite.getText().toString());
+                    flaeche =String.format("%.2f",laenge * breite);
+                    va_flaeche.setText(getString(R.string.Ergebnis_flaeche) + flaeche + getString(R.string.cm2));
                 }
                 else {
-                    int laenge = Integer.parseInt(va_laenge.getText().toString());
-                    int breite = Integer.parseInt(va_breite.getText().toString());
-                    int flaeche = laenge * breite;
-                    va_flaeche.setText("Der Flächeninhalt beträgt: " + flaeche);
+                    laenge = Float.parseFloat(va_laenge.getText().toString());
+                    breite = Float.parseFloat(va_breite.getText().toString());
+                    hoehe = Float.parseFloat(va_hoehe.getText().toString());
+                    volumen = String.format("%.1f",laenge * breite * hoehe);
+                    va_flaeche.setText(getString(R.string.Ergebnis_volmen) + volumen + getString(R.string.cm3));
                 }
             }
         });
